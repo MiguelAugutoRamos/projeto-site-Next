b102737e961e585c0e95a447521df2684d3d0a8f
MiguelAugustoRamos <miguel.augusto.ramos@escola.pr.gov.br>
2025-06-24 20:54:03 -0300

    Commit from TeleportHQ

---
+++ pages/index.js
@@ -0,0 +1,460 @@
+import React from 'react'
+import Head from 'next/head'
+
+import { useTranslations } from 'next-intl'
+
+const MacBookPro161 = (props) => {
+  const translate = useTranslations()
+  return (
+    <>
+      <div className="mac-book-pro161-container">
+        <Head>
+          <title>exported project</title>
+        </Head>
+        <div className="mac-book-pro161-mac-book-pro161">
+          <img
+            alt="Rectangle113"
+            src="/external/rectangle113-w7if-1200h.png"
+            className="mac-book-pro161-rectangle1"
+          />
+          <img
+            alt="Rectangle252"
+            src="/external/rectangle252-gt9q-200h.png"
+            className="mac-book-pro161-rectangle2"
+          />
+          <div className="mac-book-pro161-group3">
+            <span className="mac-book-pro161-text10">
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: translate.raw('text_eIZ1T9'),
+                }}
+              ></span>
+            </span>
+          </div>
+          <img
+            alt="image154"
+            src="/external/image154-u8m-200h.png"
+            className="mac-book-pro161-image1"
+          />
+          <span className="mac-book-pro161-text11">
+            <span
+              dangerouslySetInnerHTML={{
+                __html: translate.raw('text_ClIvEK'),
+              }}
+            ></span>
+          </span>
+          <span className="mac-book-pro161-text12">
+            <span
+              dangerouslySetInnerHTML={{
+                __html: translate.raw('text_wkHIKN'),
+              }}
+            ></span>
+          </span>
+          <div className="mac-book-pro161-group1">
+            <span className="mac-book-pro161-text13">
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: translate.raw('text_ZNr3Qm'),
+                }}
+              ></span>
+            </span>
+          </div>
+          <span className="mac-book-pro161-text14">
+            <span
+              dangerouslySetInnerHTML={{
+                __html: translate.raw('text_1F92gF'),
+              }}
+            ></span>
+          </span>
+          <span className="mac-book-pro161-text15">
+            <span
+              dangerouslySetInnerHTML={{
+                __html: translate.raw('text_Uq_f2g'),
+              }}
+            ></span>
+          </span>
+          <div className="mac-book-pro161-group4">
+            <img
+              alt="Rectangle5523"
+              src="/external/rectangle5523-67us-200h.png"
+              className="mac-book-pro161-rectangle5"
+            />
+            <img
+              alt="image525"
+              src="/external/image525-ufn-200h.png"
+              className="mac-book-pro161-image"
+            />
+            <span className="mac-book-pro161-text16">
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: translate.raw('text_lQJWLZ'),
+                }}
+              ></span>
+            </span>
+          </div>
+          <img
+            alt="Rectangle6530"
+            src="/external/rectangle6530-c0hr-1600w.png"
+            className="mac-book-pro161-rectangle6"
+          />
+          <img
+            alt="Rectangle7531"
+            src="/external/rectangle7531-15-1500w.png"
+            className="mac-book-pro161-rectangle7"
+          />
+          <span className="mac-book-pro161-text17">
+            <span
+              dangerouslySetInnerHTML={{
+                __html: translate.raw('text_bhA2WP'),
+              }}
+            ></span>
+          </span>
+          <span className="mac-book-pro161-text18">
+            <span
+              dangerouslySetInnerHTML={{
+                __html: translate.raw('text_aGhfe-'),
+              }}
+            ></span>
+          </span>
+          <span className="mac-book-pro161-text19">
+            <span
+              dangerouslySetInnerHTML={{
+                __html: translate.raw('text_STOXRJ'),
+              }}
+            ></span>
+          </span>
+          <span className="mac-book-pro161-text20">
+            <span
+              dangerouslySetInnerHTML={{
+                __html: translate.raw('text_Z8dJN7'),
+              }}
+            ></span>
+          </span>
+          <img
+            alt="Rectangle9741"
+            src="/external/rectangle9741-vnn-200h.png"
+            className="mac-book-pro161-rectangle9"
+          />
+          <img
+            alt="Rectangle8740"
+            src="/external/rectangle8740-te97-200h.png"
+            className="mac-book-pro161-rectangle8"
+          />
+        </div>
+      </div>
+      <style jsx>
+        {`
+          .mac-book-pro161-container {
+            width: 100%;
+            display: flex;
+            overflow: auto;
+            min-height: 100vh;
+            align-items: center;
+            flex-direction: column;
+          }
+          .mac-book-pro161-mac-book-pro161 {
+            width: 100%;
+            height: 4912px;
+            display: flex;
+            overflow: hidden;
+            position: relative;
+            align-items: center;
+            flex-shrink: 0;
+            justify-content: center;
+            background-color: rgba(31, 31, 31, 1);
+          }
+          .mac-book-pro161-rectangle1 {
+            top: 0px;
+            left: 0px;
+            width: 1728px;
+            height: 1117px;
+            position: absolute;
+          }
+          .mac-book-pro161-rectangle2 {
+            top: 52px;
+            left: 53px;
+            width: 1622px;
+            height: 94px;
+            position: absolute;
+            border-color: rgba(77, 77, 77, 1);
+            border-style: solid;
+            border-width: 2px;
+            border-radius: 20px;
+          }
+          .mac-book-pro161-group3 {
+            top: 69px;
+            left: 1474px;
+            width: 156px;
+            height: 64px;
+            display: flex;
+            position: absolute;
+            align-items: flex-start;
+            flex-shrink: 1;
+            background-color: rgba(31, 31, 31, 1);
+          }
+          .mac-book-pro161-text10 {
+            top: 14px;
+            left: 33.913063049316406px;
+            color: rgba(255, 255, 255, 1);
+            width: 88px;
+            height: auto;
+            position: absolute;
+            font-size: 32px;
+            font-style: Bold;
+            text-align: left;
+            font-family: Inter;
+            font-weight: 700;
+            line-height: normal;
+            font-stretch: normal;
+            text-decoration: none;
+          }
+          .mac-book-pro161-image1 {
+            top: 54px;
+            left: 66px;
+            width: 83px;
+            height: 83px;
+            position: absolute;
+          }
+          .mac-book-pro161-text11 {
+            top: 86px;
+            left: 697px;
+            color: rgba(255, 255, 255, 1);
+            height: auto;
+            position: absolute;
+            font-size: 24px;
+            font-style: Bold;
+            text-align: left;
+            font-family: Inter;
+            font-weight: 700;
+            line-height: normal;
+            font-stretch: normal;
+            text-decoration: none;
+          }
+          .mac-book-pro161-text12 {
+            top: 86px;
+            left: 924px;
+            color: rgba(255, 255, 255, 1);
+            height: auto;
+            position: absolute;
+            font-size: 24px;
+            font-style: Bold;
+            text-align: left;
+            font-family: Inter;
+            font-weight: 700;
+            line-height: normal;
+            font-stretch: normal;
+            text-decoration: none;
+          }
+          .mac-book-pro161-group1 {
+            top: 67px;
+            left: 1191px;
+            width: 241px;
+            height: 67px;
+            display: flex;
+            position: absolute;
+            align-items: flex-start;
+            flex-shrink: 1;
+            background-color: rgba(31, 31, 31, 1);
+          }
+          .mac-book-pro161-text13 {
+            top: 12.761903762817383px;
+            left: 23px;
+            color: rgba(255, 255, 255, 1);
+            width: 196px;
+            height: auto;
+            position: absolute;
+            font-size: 32px;
+            font-style: Bold;
+            text-align: left;
+            font-family: Inter;
+            font-weight: 700;
+            line-height: normal;
+            font-stretch: normal;
+            text-decoration: none;
+          }
+          .mac-book-pro161-text14 {
+            top: 215px;
+            left: 552px;
+            color: rgba(255, 255, 255, 1);
+            width: 624px;
+            height: auto;
+            position: absolute;
+            font-size: 48px;
+            font-style: Bold;
+            text-align: center;
+            font-family: Inter;
+            font-weight: 700;
+            line-height: normal;
+            font-stretch: normal;
+            text-decoration: none;
+          }
+          .mac-book-pro161-text15 {
+            top: 553px;
+            left: 496px;
+            color: rgba(144, 144, 144, 1);
+            width: 735px;
+            height: auto;
+            position: absolute;
+            font-size: 32px;
+            font-style: Regular;
+            text-align: center;
+            font-family: Inter;
+            font-weight: 400;
+            line-height: normal;
+            font-stretch: normal;
+            text-decoration: none;
+          }
+          .mac-book-pro161-group4 {
+            top: 694px;
+            left: 576px;
+            width: 576px;
+            height: 169.06483459472656px;
+            display: flex;
+            position: absolute;
+            align-items: flex-start;
+            flex-shrink: 1;
+          }
+          .mac-book-pro161-rectangle5 {
+            top: 17.692852020263672px;
+            left: 28.505142211914062px;
+            width: 547px;
+            height: 134px;
+            position: absolute;
+            border-radius: 27px;
+          }
+          .mac-book-pro161-image {
+            top: 0.0000271722674369812px;
+            left: -1.1368683772161603e-13px;
+            width: 169px;
+            height: 169px;
+            position: absolute;
+          }
+          .mac-book-pro161-text16 {
+            top: 41.283329010009766px;
+            left: 169.0648193359375px;
+            color: rgba(0, 0, 0, 1);
+            width: 370px;
+            height: auto;
+            position: absolute;
+            font-size: 36px;
+            font-style: Bold;
+            text-align: center;
+            font-family: Inter;
+            font-weight: 700;
+            line-height: normal;
+            font-stretch: normal;
+            text-decoration: none;
+          }
+          .mac-book-pro161-rectangle6 {
+            top: 898px;
+            left: 113px;
+            width: 1501px;
+            height: 833px;
+            position: absolute;
+            border-color: rgba(0, 0, 0, 1);
+            border-style: solid;
+            border-width: 3px;
+            border-radius: 34px;
+          }
+          .mac-book-pro161-rectangle7 {
+            top: 918px;
+            left: 130px;
+            width: 1468px;
+            height: 793px;
+            position: absolute;
+            border-radius: 34px;
+          }
+          .mac-book-pro161-text17 {
+            top: 2281px;
+            left: 648px;
+            color: rgba(255, 255, 255, 1);
+            height: auto;
+            position: absolute;
+            font-size: 48px;
+            font-style: Bold;
+            text-align: center;
+            font-family: Inter;
+            font-weight: 700;
+            line-height: normal;
+            font-stretch: normal;
+            text-decoration: none;
+          }
+          .mac-book-pro161-text18 {
+            top: 2442px;
+            left: 56px;
+            color: rgba(255, 255, 255, 1);
+            width: 1616px;
+            height: auto;
+            position: absolute;
+            font-size: 40px;
+            font-style: Bold;
+            text-align: center;
+            font-family: Inter;
+            font-weight: 700;
+            line-height: normal;
+            font-stretch: normal;
+            text-decoration: none;
+          }
+          .mac-book-pro161-text19 {
+            top: 3702px;
+            left: 157px;
+            color: rgba(255, 255, 255, 1);
+            width: 1413px;
+            height: auto;
+            position: absolute;
+            font-size: 40px;
+            font-style: Bold;
+            text-align: center;
+            font-family: Inter;
+            font-weight: 700;
+            line-height: normal;
+            font-stretch: normal;
+            text-decoration: none;
+          }
+          .mac-book-pro161-text20 {
+            top: 3540px;
+            left: 617px;
+            color: rgba(255, 255, 255, 1);
+            height: auto;
+            position: absolute;
+            font-size: 48px;
+            font-style: Bold;
+            text-align: center;
+            font-family: Inter;
+            font-weight: 700;
+            line-height: normal;
+            font-stretch: normal;
+            text-decoration: none;
+          }
+          .mac-book-pro161-rectangle9 {
+            top: 3281px;
+            left: 0px;
+            width: 1733px;
+            height: 1px;
+            position: absolute;
+          }
+          .mac-book-pro161-rectangle8 {
+            top: 2001px;
+            left: 0px;
+            width: 1733px;
+            height: 1px;
+            position: absolute;
+          }
+        `}
+      </style>
+    </>
+  )
+}
+
+export default MacBookPro161
+
+export async function getStaticProps(context) {
+  const messages = (await import('/locales/' + context.locale + '.json'))
+    .default
+  return {
+    props: {
+      messages,
+      ...context,
+    },
+  }
+}
